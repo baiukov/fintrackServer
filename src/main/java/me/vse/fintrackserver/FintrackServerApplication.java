cc64b4ba1c62d063201fe51e873e6148379b1b34
diogo <blue51@yandex.ru>
2024-10-01 14:44:38 +0200

    initial commit

---
+++ src/main/java/me/vse/fintrackserver/FintrackServerApplication.java
@@ -0,0 +1,13 @@
+package me.vse.fintrackserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintrackServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FintrackServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/vse/fintrackserver/FintrackServerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.vse.fintrackserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintrackServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
