f1c8227fabc827bea4d004d6e00c7f254bc4e1d2
diogo <blue51@yandex.ru>
2024-10-07 20:16:30 +0200

    fix db

--- src/main/java/me/vse/fintrackserver/FintrackServerApplication.java
+++ src/main/java/me/vse/fintrackserver/FintrackServerApplication.java
@@ -1,12 +1,20 @@
 package me.vse.fintrackserver;
 
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FintrackServerApplication {
 
+    @Autowired
+    private static EntityManager entityManager;
+
     public static void main(String[] args) {
+
+//        Test test = new Test(1L, "icon", "name", "color", null, null);
+//        entityManager.persist(test);
         SpringApplication.run(FintrackServerApplication.class, args);
     }
 
